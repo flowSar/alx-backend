b97c2e6019aaf050a13f292a40ace77f361d2bfa
flowsar <brahim.sar151@gmail.com>
2024-10-31 00:38:06 +0100

    MRU

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
-"""Cache replacement policies - LRUCache"""
+"""Cache replacement policies - MRU"""
 BaseCaching = __import__("base_caching").BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """Cache replacement policies - LRUCache"""
+class MRUCache(BaseCaching):
+    """Cache replacement policies - MRU"""
 
     def __init__(self):
         """initialize cache_data"""
         super().__init__()
 
     def put(self, key, item):
-        """insert new item and discard the least recently used  if
+        """insert new item and discard the MRU when
         len(cache_data) > BaseCaching.MAX_ITEMS"""
         if key and item:
             if key in self.cache_data:
                 self.cache_data.pop(key)
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            elif len(self.cache_data) >= self.MAX_ITEMS:
                 last = self.cache_data.popitem()
                 print('DISCARD: ', last[0])
+                
             self.cache_data[key] = item
 
     def get(self, key):
