b8f823dc5f24bdbc7b4224b72ad4ce307f67fac6
flowsar <brahim.sar151@gmail.com>
2024-10-31 00:21:48 +0100

    the least recently used

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Cache replacement policies - LRUCache"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Cache replacement policies - LRUCache"""
+
+    def __init__(self):
+        """initialize cache_data"""
+        super().__init__()
+
+    def put(self, key, item):
+        """insert new item and discard the least recently used  if
+        len(cache_data) > BaseCaching.MAX_ITEMS"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.order.pop(0)
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """get cache_data"""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
