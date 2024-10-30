64be453c25ea2fdcf00a42434ea4e6292a28c6fc
flowsar <brahim.sar151@gmail.com>
2024-10-31 00:18:11 +0100

    LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -27,18 +27,3 @@ class LIFOCache(BaseCaching):
         if key is not None:
             return self.cache_data.get(key)
         return None
-
-my_cache = LIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
