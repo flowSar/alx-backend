4c896b2f4296a6344d8044bbd50d240b94e9bea9
flowsar <brahim.sar151@gmail.com>
2024-11-01 23:51:21 +0100

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Simple helper function """
+    startPage = (page - 1) * page_size
+    endPage = page * page_size
+    return (startPage, endPage)
