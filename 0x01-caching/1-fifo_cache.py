2261b9e4e8d4241fe6837d8a018bd9196509f2a0
flowsar <brahim.sar151@gmail.com>
2024-11-02 00:40:24 +0100

    fic FIFO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,17 +14,12 @@ class FIFOCache(BaseCaching):
         """insert new item and discard the first item if
         len(cache_data) > BaseCaching.MAX_ITEMS"""
         if key and item:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                if self.cache_data.get(key) is None:
-                    first_key = list(self.cache_data.keys())[0]
-                    del self.cache_data[first_key]
-                    self.cache_data[key] = item
-                    print('DISCARD: ', first_key)
-
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = next(iter(self.cache_data))
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
             self.cache_data[key] = item
 
     def get(self, key):
         """get cache_data"""
-        if key is not None:
-            return self.cache_data.get(key)
-        return None
+        return self.cache_data.get(key)
