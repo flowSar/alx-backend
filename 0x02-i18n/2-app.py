6835a46e64e45d29118482490486358f2a5bbb28
flowsar <brahim.sar151@gmail.com>
2024-11-05 23:54:46 +0100

    best match with our supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""flask server"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    # Class attribute for available language
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home():
+    """home route"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=1111, debug=True)
