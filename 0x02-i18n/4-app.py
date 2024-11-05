621a338aefe20688b0645f92733dca63d90945bf
flowsar <brahim.sar151@gmail.com>
2024-11-06 00:37:48 +0100

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,13 +19,17 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """get best match with our supported languages"""
+
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
 def home():
     """home route"""
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
