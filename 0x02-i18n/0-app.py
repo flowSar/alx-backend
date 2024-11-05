63054be3737058bc8c16ac2be44d8cd753d80fd7
flowsar <brahim.sar151@gmail.com>
2024-11-05 23:47:38 +0100

    instantiate the Babel object

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 """flask server"""
 from flask import Flask, render_template
-from flask_babel import Babel
-
-
-class Config:
-    # Class attribute for available language
-    LANGUAGES = ["en", "fr"]
 
 
 app = Flask(__name__)
-babel = Babel(app)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
-app.config.from_object(Config)
 
 
 @app.route("/")
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""flask server"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    # Class attribute for available language
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object(Config)
+
+
+@app.route("/")
+def home():
+    """home route"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=1111, debug=True)
